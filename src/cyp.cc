7f4e3a1266167e66413a5a95f681dcd510102f4b
Ondrej Holecek <ondrej.holecek@firma.seznam.cz>
2014-10-07 17:14:26 +0200

    Revert "minor fix: while cycle condition"
    This reverts commit dd4ae25a4b0d979262f597a4f3d1913cd299c246.

--- src/cyp.cc
+++ src/cyp.cc
@@ -144,7 +144,8 @@ void randomize_with_cypher(BlockCypher_t &bc, Ptr_t &ptr) {
 template<typename Cypher_t>
 void process_mem(Cypher_t &cyp, char * in, char * out, size_t size) {
     typename Cypher_t::Context_t ctx;
-    while (size < Cypher_t::BLOCK_SIZE) {
+    while(size) {
+        if (size < Cypher_t::BLOCK_SIZE) return;
         cyp.process(in, out, ctx);
         in += Cypher_t::BLOCK_SIZE;
         out += Cypher_t::BLOCK_SIZE;
